5b156b72b0a28e472822d8b34d2389d4445c4b89
Administrator <Administrator@DELL>
2020-07-01 14:48:28 +0530

    Project guide file is added.

--- SpringCoreBeanLifeCycleInitializingDisposableCallbacks/src/main/java/com/javalive/beans/Person.java
+++ SpringCoreBeanLifeCycleInitializingDisposableCallbacks/src/main/java/com/javalive/beans/Person.java
@@ -3,6 +3,10 @@ package com.javalive.beans;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
+/**
+ * @author Administrator
+ *
+ */
 public class Person implements InitializingBean,DisposableBean{
 	 
 	   private String name;
@@ -10,13 +14,13 @@ public class Person implements InitializingBean,DisposableBean{
 	   {
 	      System.out.println("Constructor of person bean is called !! ");
 	   }
-	   public void destroy() throws Exception
-	   {
-	     System.out.println("destroy method of person bean is called !! ");
-	   }
-	   public void afterPropertiesSet() throws Exception 
+	   public void afterPropertiesSet() throws Exception // this is method of InitializingBean interface.
 	   {
 	   System.out.println("afterPropertiesSet method of person bean is called !! ");
+	   }
+	   public void destroy() throws Exception // this is method of DisposableBean interface.
+	   {
+	     System.out.println("destroy method of person bean is called !! ");
 	   }
 	   public String getName() {
 	    return name;
